197620341acc35b48a1033ffa556a00b248899a9
denghao <982705413@qq.com>
2020-05-12 19:16:25 +0800

    登录功能完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,15 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
+    <div>
+        home
     </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'Home'
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
